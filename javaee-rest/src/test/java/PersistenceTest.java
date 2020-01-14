2f3aa17bbe2cff5c04c8e49bbcf2e4a5577a7e88
Vlad Flore <flore.vlad@gmail.com>
2020-01-15 00:13:23 +0100

    persistence unit available from junit test

--- javaee-rest/src/test/java/PersistenceTest.java
+++ javaee-rest/src/test/java/PersistenceTest.java
@@ -5,12 +5,15 @@ import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 public class PersistenceTest {
 
     @Test
     @Disabled
     void test() {
-        EntityManagerFactory emf = Persistence.createEntityManagerFactory("default");
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("default-pu");
         EntityManager em = emf.createEntityManager();
+        assertThat(em).isNotNull();
     }
 }
